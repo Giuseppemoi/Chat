b0fe1e413dc2e49f138fea48600184f20ea03464
evrardsibo <evrard.sibomana@gmail.com>
2021-09-28 16:51:10 +0200

    chat

---
+++ serveur.js
@@ -0,0 +1,23 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get("/",(req , res) =>{
+    res.sendFile(__dirname + '/test.html')
+})
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnect');
+    })
+    socket.on('chat message', (msg) => {
+        console.log('message recu  : ' + msg);
+        io.emit('chat message', msg);
+    })
+})
+
+http.listen(3000, () => {
+
+    console.log('connected')
+})
