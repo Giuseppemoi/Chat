e607bda14157de57aa72cade8d9d1c8679dc47ef
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-09-29 10:00:41 +0200

    modif js

---
+++ assets/js/socket.js
@@ -0,0 +1,16 @@
+let socket = io('http://localhost:3000/');
+let button = document.getElementById('submit')
+
+let send = function(){
+    let text = document.getElementById("input").value;
+    socket.emit('chat message',text)
+}
+
+button.addEventListener("click", send)
+
+let recu = function(msg){
+    let li = document.createElement('li');
+    li.innerText = msg;
+    document.getElementById('messages').appendChild(li);
+}
+socket.on('chat message', recu);
