1827d7c2affd90bea8337393a53d52bb3d473500
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-09-30 15:24:48 +0200

    fix bug

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,39 +1,37 @@
-<<<<<<< HEAD:assets/js/client.js
 let socket = io();
 let button = document.getElementById('submit')
+
 let send = function(){
     let text = document.getElementById("input").value;
     socket.emit('chat message',text)
 }
+
 button.addEventListener('click', send)
+
 let recu = function(msg){
-    let li = document.createElement('li');
-    li.innerText = msg;
-    document.getElementById('messages').appendChild(li);
+    let div = `<div class="sendmessageuser1">
+                        <img class="avatar" src="" alt="avatar">
+                        <p class="textmessage">${msg}</p>
+                    </div>`;
+    document.getElementById('messages').innerHTML += div
+    // let li = document.createElement('li');
+    // li.innerText = msg;
+    // document.getElementById('messages').appendChild(li);
 }
+
 socket.on('chat message', recu);
+
 socket.on('output-messages', data => {
     if (data.length) {
         data.forEach(message => {
-            let li = document.createElement('li');
-            li.innerText = message.msg;
-            document.getElementById('messages').appendChild(li);
+            let div = `<div class="sendmessageuser1">
+                            <img class="avatar" src="img/avatar1.png" alt="avatar">
+                            <p class="textmessage">${message.msg}</p>
+                       </div>`;
+            document.getElementById('messages').innerHTML += div
+            // let li = document.createElement('li');
+            // li.innerText = message.msg;
+            // document.getElementById('messages').appendChild(li);
         })
     }
 })
-=======
-let socket = io('http://localhost:3000');
-       let button = document.getElementById('submit')
-          let send = function(){
-              let text = document.getElementById("input").value;
-              socket.emit('chat message',text)
-          }
-          button.addEventListener("click", send)
-          let recu = function(msg){
-              let li = document.createElement('li');
-              li.innerText = msg;
-              document.getElementById('messages').appendChild(li);   
-          }
-          socket.on('chat message', recu);
-        
->>>>>>> master:socket.js
--- assets/js/registerUser.js
+++ assets/js/registerUser.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require('mongoose')
 const User = require("../model/userSchema");
 
